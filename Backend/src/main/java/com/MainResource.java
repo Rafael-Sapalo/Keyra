301b486bca9ef6df4b2dcae01daf4c7ce9e3927d
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-05-20 19:08:00 +0200

    feat: configure HTTP port and implement custom Quarkus application startup

--- Backend/src/main/java/com/MainResource.java
+++ Backend/src/main/java/com/MainResource.java
@@ -1,11 +1,21 @@
 package com;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class MainResource {
     public static void main(String... args) {
-        Quarkus.run(args);
+        Quarkus.run(KeyraApp.class, args);
+    }
+
+    public  static class KeyraApp implements QuarkusApplication {
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("🚀 Keyra is starting...");
+            Quarkus.waitForExit();
+            return 0;
+        }
     }
 }
