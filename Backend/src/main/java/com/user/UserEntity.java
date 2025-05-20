265f107ce337bdbf2c4611f51a4270f16958387a
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-05-20 19:57:19 +0200

    feat: add Quarkus dependencies for Hibernate ORM and PostgreSQL support

---
+++ Backend/src/main/java/com/user/UserEntity.java
@@ -0,0 +1,73 @@
+package com.user;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "UUID", updatable = false, nullable = false)
+    public UUID id;
+
+    @Column(length = 255, nullable = false, unique = true)
+    public String email;
+
+    @Column(length = 255, nullable = false)
+    public String username;
+
+    @Column(name = "password", nullable = false)
+    public String password;
+
+    @Column(name = "first_name", length = 255)
+    public String firsName;
+
+    @Column(name = "last_name", length = 255)
+    public String lastName;
+
+    @Column(name = "phone_number", length = 20)
+    public String phoneNumber;
+
+    @Column(columnDefinition = "TEXT")
+    public String bio;
+
+    @Column(columnDefinition = "TEXT")
+    public String pfp;
+
+    @Column(name = "is_verified", nullable = false)
+    public boolean isVerified = false;
+
+    @Column(name = "is_active", nullable = false)
+    public boolean isActive = true;
+
+    @Column(name = "is_banned", nullable = false)
+    public boolean isBanned = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    public LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    public LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void prePersist() {
+        if (id == null) id = UUID.randomUUID();
+        if (createdAt == null) createdAt = LocalDateTime.now();
+        if (updatedAt == null) updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        if (updatedAt == null) updatedAt = LocalDateTime.now();
+    }
+}
