571f2d3ec79051e2b8e8f4bc4e4e03d7eaed7ae2
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-06-06 14:42:11 +0200

    feat: update login endpoint to return a LoginResponse object

--- Backend/src/main/java/com/backend/auth/AuthController.java
+++ Backend/src/main/java/com/backend/auth/AuthController.java
@@ -1,17 +1,19 @@
 package com.backend.auth;
 
+import com.backend.auth.dto.LoginResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("auth")
 public class AuthController {
 
     @PostMapping(value = "login", produces = "application/json")
-    public String login() {
-        return "login";
+    public ResponseEntity<LoginResponse> login() {
+        return ResponseEntity.ok(new LoginResponse("TOKEN"));
     }
 
     @PostMapping(value = "refresh", produces = "application/json")
---
+++ Backend/src/main/java/com/backend/auth/dto/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.backend.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class LoginResponse {
+    @JsonProperty("access_token")
+    private String token;
+}
