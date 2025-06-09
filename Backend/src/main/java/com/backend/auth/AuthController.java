0e16c8bbd5605c9139e8159785ceb5189a5e41b1
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-06-09 16:35:12 +0200

    feat: remove unused HttpStatus import in AuthController

--- Backend/src/main/java/com/backend/auth/AuthController.java
+++ Backend/src/main/java/com/backend/auth/AuthController.java
@@ -1,7 +1,6 @@
 package com.backend.auth;
 
 import com.backend.auth.dto.*;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
